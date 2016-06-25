28c1c0e837ae57121cb62f0844144d0da46b2fe9
Sergio NS <eng.sergio.ns@gmail.com>
2016-06-25 10:30:26 -0500

    Changes jade to pug and moves favicons to img

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('logger', logger);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
